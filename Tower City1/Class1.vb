0c081754e8dc496875d29f36593e682ee82d0497
Ryan McNeil <mcneil.ryan.m@gmail.com>
2011-04-07 14:24:00 -0400

    Committing project files

---
+++ Tower City1/Class1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Class1
+    'Time controls
+    Public Hour As Short
+    Public MinuteTens As Short
+    Public MinuteSingle As Short
+    Public PM As Boolean
+
+    'Financial information
+    Public CurrentMoney As Integer
+    Public TotalIncome As Integer
+    Public TotalExpenses As Integer
+
+    'Income
+    Public RentIncome As Integer
+    Public HotelIncome As Integer
+    Public FastFoodIncome As Integer
+    Public RestaurantIncome As Integer
+    Public TheaterIncome As Integer
+    Public BallroomIncome As Integer
+    Public DaycareIncome As Integer
+    Public MallIncome As Integer
+End Class
